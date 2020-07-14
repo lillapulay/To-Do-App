9ff6d6e2ecdc65fc1a27b1af06c8e116d6cb9125
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-07-14 17:17:40 +0200

    Imported MdCard, MdButton and MdCheckbox

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdField } from 'vue-material/dist/components';
+import { MdField, MdCard, MdButton, MdCheckbox } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(MdField);
+Vue.use(MdCard);
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
 
 Vue.config.productionTip = false
 
