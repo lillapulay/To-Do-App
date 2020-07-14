957d0197807994c91d12499e405933be0020fc04
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-07-14 19:04:25 +0200

    Added edit function, buttons and styling with Material

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,105 @@
 <template>
-  <div>
-    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.label }}
-      </li>
-    </ul>
-    <button @click="removeTodo(todo)">Delete</button>
+  <div class="container">
+    <h1>To-do list</h1>
+    <md-field>
+      <md-input 
+        v-model="currentTodo" 
+        @keydown.enter="addTodo()" 
+        placeholder="What do you need to do?">
+      </md-input>
+    </md-field>
+
+    <div class="todo-list">
+      <ul class="todos">
+        <li 
+          class="todo-item" 
+          v-for="todo in todos" 
+          :key="todo.id">
+          <span>
+            <input
+              class="completed"
+              type="checkbox"
+              @change="togglecompleted"
+              v-model="todo.completed"
+            />
+
+            <span
+              v-if="todo.editing === false"
+              class="todo-item-label"
+              :class="{'is-complete': todo.completed}"
+              @dblclick="editTodo(todo)">
+            {{ todo.label }}
+            </span>
+            
+            <div v-if="todo.editing">
+              <md-field>
+                <md-input
+                  type="text"
+                  v-model="todo.label"
+                  @keyup.enter="cancelEdit(todo)"
+                  @keyup.esc="cancelEdit(todo)"
+                  @focusout="cancelEdit(todo)"
+                  placeholder="Edit this to-do">
+                </md-input>
+              </md-field>
+            </div>
+          </span>
+
+          <md-button 
+            class="md-raised 
+            md-ripple:false" 
+            @click="editTodo(todo)">
+            Edit
+          </md-button>
+
+          <md-button 
+            class="md-raised md-accent" 
+            @click="removeTodo(todo)">
+            Delete
+          </md-button>
+
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
 
 <script>
 export default {
   data() {
     return {
       todos: [],
-      currentTodo: ''
+      currentTodo: "",
+      editedTodo: ""
     };
   },
   methods: {
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-      this.currentTodo = '';
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        editing: false
+      });
+      this.currentTodo = "";
+    },
+    togglecompleted() {
+      this.todos.completed = !this.todos.completed;
     },
     removeTodo(todo) {
       var index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      todo.editing = true;
+    },
+    cancelEdit(todo) {
+      todo.editing = false;
     }
   }
 };
 </script>
 
 <style>
-</style>
+</style> 
